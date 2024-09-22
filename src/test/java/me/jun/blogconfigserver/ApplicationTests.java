ecb8c0b62b298dae0bc42c91272f197923726972
kjh9267 <intellijunho@kakao.com>
2024-09-22 21:03:25 +0900

    initial commit

---
+++ src/test/java/me/jun/blogconfigserver/ApplicationTests.java
@@ -0,0 +1,13 @@
+package me.jun.blogconfigserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
